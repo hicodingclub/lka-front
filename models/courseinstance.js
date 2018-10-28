9975216f51826d7473721533fa2fbde3a4d2eba9
Eric Sheng <ericsheng@Erics-MacBook-Pro.local>
2018-10-27 21:59:42 -0700

    change models

--- models/courseinstance.js
+++ models/courseinstance.js
@@ -2,19 +2,27 @@ var mongoose = require('mongoose');
 
 var Schema = Course.Schema;
 
-var CourseinstanceSchema = new Schema(
+var courseInstanceSchema = new Schema(
   {
-    Courseinstance_title: {type: String, required: true},
-    Courseinstance_description: {type: String, required: true, max: 100},
-    Courseinstance_Room: {type: int, required: true, max: 100},
-    Courseinstance_price: {type:float, required: true, max: 100},
-    Courseinstance_time: {type: float, required: true, max: 100},
-    Courseinstance_teacher: {type: String, required: true, max: 100},
-    Courseinstance_price: {type:float, required: true, max: 100},
-    Course: { type: Schema.Types.ObjectId, ref: 'Course', required: true }, 
-    Teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', required: true }, 
+    courseInstance_title: {type: String, required: true},
+    courseInstance_description: {type: String, required: true, max: 100},
+    courseInstance_Room: {type: int, required: true, max: 100},
+    courseInstance_price: {type:float, required: true, max: 100},
+    courseInstance_time: {type: float, required: true, max: 100},
+    courseInstance_teacher: {type: String, required: true, max: 100},
+    courseInstance_price: {type:float, required: true, max: 100},
+    course: { type: Schema.Types.ObjectId, ref: 'Course', required: true }, 
+    teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', required: true }, 
   }
 );
 
 //Export model
-module.exports = course.model('Courseinstance', CourseinstanceSchema);
+module.exports = mongoose.model('Courseinstance', courseInstanceSchema);
+
+
+//Virtual for couseInstance's full name
+couseInstanceSchema
+.virtual('name')
+.get(function () {
+  return this.last_name + ', ' + this.first_name;
+});
