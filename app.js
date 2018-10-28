0e125d0768dc9f7cd4b35ce6e4338b44308e147c
andrewnpaul <andrew.paul@warriorlife.net>
2018-10-27 21:36:16 -0700

    add database connection

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://lkauser:lkauser1@ds143683.mlab.com:43683/lka';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
