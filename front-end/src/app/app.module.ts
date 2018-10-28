781d339fc15d434a9f2ac4cb216b5a34e1e3d87e
Xibin Liu <xibinliu@cienet.com.cn>
2018-10-27 23:40:23 -0700

    Modify Angular back-end to show data

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ManageModule } from './manage/manage.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    ManageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
