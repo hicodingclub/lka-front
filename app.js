1af934dfe857bf787592a9b3dff1c7f7b9ca33d7
Xibin Liu <xibinliu@cienet.com.cn>
2018-10-27 23:36:54 -0700

    Get static file from public and default is index.html

--- app.js
+++ app.js
@@ -37,6 +37,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', lkaRouter);
 
+app.use(function(req, res, next) {
+	res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
