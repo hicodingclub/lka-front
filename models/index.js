60b66daa2012fb78570ec20533897dfcac5a3d77
Jack <toliujiayi@gmail.com>
2018-11-03 21:33:14 -0700

    export different views in the teacher model and add the view in index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 
 let eventDef = require('./event');
+let teacherDef = require('./teacher');
 
 var schemas = {
   "event": eventDef,
+  "teacher": teacherDef,
 };
 
 var config = {
