871d8d8e1ecf62a634c8d636b8c6f60d6513e429
andrewnpaul <andrew.paul@warriorlife.net>
2018-11-03 21:45:21 -0700

    add student model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 
 let eventDef = require('./event');
+let studentDef = require('./student');
 
 var schemas = {
   "event": eventDef,
+  "student": studentDef,
 };
 
 var config = {
--- models/student.js
+++ models/student.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var StudentSchema = new Schema(
+var studentSchema = new Schema(
   {
     first_name: {type: String, required: true, max: 100},
     last_name: {type: String, required: true, max: 100},
@@ -13,12 +13,25 @@ var StudentSchema = new Schema(
 );
 
 // Virtual for Student's full name
-StudentSchema
+studentSchema
 .virtual('name')
 .get(function () {
   return this.last_name + ', ' + this.first_name;
 });
 
+var studentBrief = "first_name last_name";
+var studentDetail = "first_name last_name date_of_birth email grade";
+var studentCreat = "first_name last_name grade";
+var studentEdit = "first_name last_name grade";
+var studentTextSearch = "first_name last_name grade";
+var studentIndex = "name";
+
+var views = [studentBrief, studentDetail, studentCreat, studentEdit, studentTextSearch, studentIndex]
 
 //Export model
-module.exports = mongoose.model('Student', StudentSchema);
+//module.exports = mongoose.model('Student', StudentSchema);
+
+
+module.exports = {schema: studentSchema, views: views};
+
+
