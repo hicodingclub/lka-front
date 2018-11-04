a0e16add370210dd0cffc78c9000b88aa183b4ce
Eric Sheng <ericsheng@Erics-MacBook-Pro.local>
2018-11-03 21:45:22 -0700

    add course schema definition

--- models/index.js
+++ models/index.js
@@ -2,10 +2,13 @@ var mongoose = require('mongoose');
 
 let eventDef = require('./event');
 let teacherDef = require('./teacher');
+let courseDef = require('./course');
 
 var schemas = {
   "event": eventDef,
   "teacher": teacherDef,
+  "course": courseDef,
+
 };
 
 var config = {
