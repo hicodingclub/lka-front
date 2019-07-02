7f0d14c60603c8454411a3366cac162b372feaf2
Xibin Liu <xibinliu@yahoo.com>
2019-07-01 23:33:41 -0700

    Remove the old 'home' implementation

--- front-end/src/app/home/public-routing.ts
+++
@@ -1,7 +0,0 @@
-import { ClassDetailComponent } from '../academics/class/class-detail/class-detail.component';
-import { EventDetailComponent } from '../publicinfo/event/event-detail/event-detail.component';
-
-export const publicRoutingPath = [
-     {path: 'class/:id', component: ClassDetailComponent},
-     {path: 'event/:id', component: EventDetailComponent},
-];
