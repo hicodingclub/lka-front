6d25bb6053b596e1b33ff909066d5e6ecb42fcd4
Xibin Liu <xibinliu@yahoo.com>
2019-07-19 19:46:18 -0700

    model minor change: fqa has hidden 'order' field

--- models/publicInfo/faq-public.js
+++ models/publicInfo/faq-public.js
@@ -1,6 +1,6 @@
 const schema = require('./faq');
 
-const Brief = "question answer";
+const Brief = "question answer (order)";
 const Detail = "question | answer ";
 const Creat = "question answer order enable";
 const Edit = "question answer order enable";
