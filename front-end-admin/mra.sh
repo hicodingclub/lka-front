94cb3bc652bfc23060c67d9f01e8571a5c8c5a0d
Xibin Liu <xliu@cienet.com>
2019-10-12 23:01:52 -0700

    update version to 0.7.100

--- front-end-admin/mra.sh
+++ front-end-admin/mra.sh
@@ -1,4 +1,6 @@
 ~/dev/mean-rest/src/mean-rest-angular-cli/bin/mra.js -m publicinfo -o src/app ../../lka/models/publicInfo/index.js
+~/dev/mean-rest/src/mean-rest-angular-cli/bin/mra.js -m emailtemplate -o src/app ../../lka/models/emailTemplate/model.js
+
 ~/dev/mean-rest/src/mean-rest-angular-cli/bin/mra.js -m access -o src/app ../../booklib/auth/model.access.js
 ~/dev/mean-rest/src/mean-rest-angular-cli/bin/mra.js -m roles -o src/app ../../booklib/auth/model.role.js
 ~/dev/mean-rest/src/mean-rest-angular-cli/bin/mra.js -m academics -o src/app ../../lka/models/academics/index.js
